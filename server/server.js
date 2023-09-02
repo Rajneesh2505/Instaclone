1746a14c37aac7376cf6fc13e279e0917a3b0ca6
rajneesh25 <rajneeshdadheech2505@gmail.com>
2023-09-02 14:06:30 +0530

    Done

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express=require("express")
+const app=express()
+const mongoose=require("mongoose")
+const userController=require("./routes/user")
+const multer=require("multer")()
+const cors=require("cors")
+const postController=require("./routes/post")
+
+
+//Database connection
+mongoose.connect("mongodb://127.0.0.1:27017/insta")
+
+
+//body parser middleware
+app.use(express.json({limit:"10mb",extended:true}))
+app.use(express.urlencoded({extended:false}))
+app.use(multer.array())
+app.use(cors())
+//Server
+app.listen(3000,(req,err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("server start")
+    }
+})
+
+
+
+//middleware
+app.use("/user",userController)
+app.use("/post",postController)
